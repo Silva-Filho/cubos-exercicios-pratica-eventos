969b9eead84f60c8f5e9c7100c4cb032ea7a65b6
Silva Filho <tarusilva@gmail.com>
2022-07-21 18:12:53 -0300

    Atualização da lógica de mostrar ou esconder as setas do modal.

--- classe-01/script.js
+++ classe-01/script.js
@@ -14,6 +14,8 @@ const imagemWrapperModal = document.querySelectorAll(".imagem-wrapper-modal img"
 const imagemModal = document.querySelector(".imagem-modal");
 const setaParaEsquerda = document.querySelector(".imagem-wrapper-modal img:first-child");
 /* console.log(setaParaEsquerda); */
+const setaParaDireita = document.querySelector(".imagem-wrapper-modal img:last-child");
+/* console.log(setaParaDireita); */
 
 const imgFecharMenu = "assets/open-menu.svg";
 const imgAbrirMenu = "assets/closed-menu.svg";
@@ -21,49 +23,77 @@ const imgAbrirMenu = "assets/closed-menu.svg";
 // Manipular aba menu:
 const manipularMenu = () => {
     const menuSrc = imgMenu.getAttribute("src");
-    /* console.log(menuSrc); */
     const imgSrc = menuSrc === imgAbrirMenu ? imgFecharMenu : imgAbrirMenu;
-    /* console.log(imgSrc); */
 
     header.classList.toggle("expandir-header");
-    h2.forEach( elemento => elemento.classList.toggle("escondido") );
+    h2.forEach(elemento => elemento.classList.toggle("escondido"));
     imgMenu.setAttribute("src", imgSrc);
-    /* console.log(imgMenu); */
 };
 
-imgMenu.addEventListener("click", manipularMenu);
+imgMenu?.addEventListener("click", manipularMenu);
 
 // Manipular imagem a ser mostrada no modal:
 const abrirModal = element => {
+    console.log({ index: element.dataset.index });
     const src = element.src;
     const previousElement = element.previousElementSibling;
-    /* console.log(previousElement); */
     const nextElement = element.nextElementSibling;
-    /* console.log(nextElement); */
 
-    imagemModal.setAttribute("src", src);
+    imagemModal?.setAttribute("src", src);
     modal.style.display = "flex";
-
 };
 
-/* const selecionarImagem = event => abrirModal(event.target.src); */
-const selecionarImagem = event => abrirModal(event.target);
+const selecionarImagem = event => {
+    /* console.log({event: event}); */
+    abrirModal(event.target)
+};
 
-const imagemSelecionada = imagem => imagem.addEventListener("click", selecionarImagem);
+/* const imagemSelecionada = imagem => imagem.addEventListener("click", selecionarImagem);
 
-imagens.forEach(imagemSelecionada);
+imagens.forEach(imagemSelecionada); */
+
+imagemWrapper.forEach(item => {
+    /* console.log(item); */
+    /* const imagem = document.querySelector("img"); */
+    const imagem = item.querySelector("img");
+    /* console.log(imagem); */
+
+    imagem?.addEventListener("click", event => {
+        console.log({ index: event.target?.dataset.index });
+        console.log({ id: Number(event.target?.dataset.id) });
+        const src = event.target?.src;
+        /* const previousElement = event.target?.previousElementSibling;
+        const nextElement = event.target?.nextElementSibling; */
+        const previousElement = item.previousElementSibling;
+        console.log({ previousElement: previousElement });
+        previousElement ?
+            setaParaEsquerda?.classList.remove("escondido") :
+            setaParaEsquerda?.classList.add("escondido");
+
+        const nextElement = item.nextElementSibling;
+        console.log({ nextElement: nextElement });
+        nextElement ?
+            setaParaDireita?.classList.remove("escondido") :
+            setaParaDireita?.classList.add("escondido");
+
+        imagemModal?.setAttribute("src", src);
+        modal.style.display = "flex";
+    });
+});
 
 // Fechar o modal:
 const sairModal = () => modal.style.display = "none";
 
-modal.addEventListener("click", sairModal);
+modal?.addEventListener("click", sairModal);
 
 // Fechar o modal ao clicar fora da imagem:
 const manterModalAberto = (event) => event.stopPropagation();
 
-imagemWrapperModal.forEach( imagem => {
+/* imagemWrapperModal.forEach( imagem => {
     imagem.addEventListener("click", manterModalAberto);
-} );
+} ); */
+
+imagemModal?.addEventListener("click", manterModalAberto);
 
 // Trocar para imagem anterior se houver:
 
